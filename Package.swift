b208cabfbb352bbc51a586a9d247eaa678f8bacd
Antwan van Houdt <jabwd@exurion.com>
2017-11-03 14:14:20 +0100

    Removed event2 header files in favor of locally installed ones

--- Package.swift
+++ Package.swift
@@ -18,12 +18,9 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target defines a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CEvent",
-            dependencies: [],
-            publicHeadersPath: "."
+            dependencies: []
         ),
     ]
 )
