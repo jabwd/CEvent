149d1bcd8510415b469463e8ed37b1aac5fc40df
Antwan van Houdt <jabwd@exurion.com>
2017-11-03 13:56:40 +0100

    Typo fix in package config

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .target(
             name: "CEvent",
             dependencies: [],
-            publicHeaders: "."
+            publicHeadersPath: "."
         ),
     ]
 )
